3ecd1dab15c1414e653ff8b1fbf396f869a53901
karl bertin <bertin_k@qwasar.io>
2021-05-15 22:13:47 +0000

    [quest03/] ex00/ updated: finished Reverse String.

--- ex00/reverse_string.c
+++ ex00/reverse_string.c
@@ -8,18 +8,35 @@
 **
 */
 
+#include <string.h>
+/*#include <stdio.h>*/
+
 char* reverse_string(char* param_1)
 {
+    char* s = param_1;
+	char c;
+	int l = strlen((char*) s);
+	int i = 0;
+	int j = l - 1;
 
+    while ( i < j )
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+        i++;
+        j--;
+    }
+    return s;
 }
 
 /*
 Example of main
 */
-int main() {
+/*int main() {
   char *my_str = strdup("Hello");
   
   printf("Before reverse -> %s\n", my_str);
   printf("Reverse -> %s\n", reverse_string(my_str));
   return 0;
-}
+}*/
