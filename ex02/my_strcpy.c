c32d1e1585145489974ecb7257320fed94740789
karl bertin <bertin_k@qwasar.io>
2021-05-15 23:38:24 +0000

    [quest03/] ex02/ updated: finished My STRCPY.

--- ex02/my_strcpy.c
+++ ex02/my_strcpy.c
@@ -9,15 +9,23 @@
 **
 */
 
-char* my_strcpy(char* param_1, char* param_2)
+char* my_strcpy(char* param_1, char* param_2) //char* strcpy(char* dst, const char* src)
 {
+	char* s_0 = param_1;
+    int i = 0;
 
+	while ( param_2[i] != 0 ) //while ( param_1[i] == param_2[i] && param_1[1] != 0 && param_2[i] != 0 )
+    {
+		s_0[i] = param_2[i];
+        i++;
+    }
+    return s_0;
 }
 
 /*
 Example of main
 */
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
   char dst[100] = {0};
@@ -25,4 +33,4 @@ int main() {
   
   printf("my_strcpy -> %s\n", my_strcpy(dst, src));
   return 0;
-}
+}*/
