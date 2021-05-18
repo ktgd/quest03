274dcb98863250e4d0829ccd902539e190f037df
karl bertin <bertin_k@qwasar.io>
2021-05-18 23:54:26 +0000

    [quest03/] ex04/ updated: finished My STRCHR.

--- ex02/my_strcpy.c
+++ ex02/my_strcpy.c
@@ -11,15 +11,17 @@
 
 char* my_strcpy(char* param_1, char* param_2) //char* strcpy(char* dst, const char* src)
 {
-	char* s_0 = param_1;
-    int i = 0;
+	char* s_1 = param_1;
+	char* s_2 = param_2;
+	char* s_0 = s_1;
+	int i = 0;
 
-	while ( param_2[i] != 0 ) //while ( param_1[i] == param_2[i] && param_1[1] != 0 && param_2[i] != 0 )
-    {
-		s_0[i] = param_2[i];
-        i++;
-    }
-    return s_0;
+	while ( s_2[i] != 0 ) //while ( param_1[i] == param_2[i] && param_1[1] != 0 && param_2[i] != 0 )
+	{
+		s_0[i] = s_2[i];
+		i++;
+	}
+	return s_0;
 }
 
 /*
--- ex04/my_strchr.c
+++ ex04/my_strchr.c
@@ -9,7 +9,30 @@
 **
 */
 
+#include <stddef.h>
+//#include <stdio.h>
+
 char* my_strchr(char* param_1, char param_2)
 {
+	char* s = param_1;
+	char c = param_2;
+	int i = 0;
 
+	while ( s[i] != c )
+	{
+		if ( s[i] == 0 )
+		{
+			return NULL;
+		}
+		i++;
+	}
+	return (char*) &s[i];
 }
+
+/*int main()
+{
+	char* s = "Hi. My name is Karl.";
+	char c = 'K';
+
+	printf("%s\n", my_strchr(s, c));
+}*/
