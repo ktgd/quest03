22a913f27110783d05498703f29a0793971e97a7
karl bertin <bertin_k@qwasar.io>
2021-05-20 23:46:49 +0000

    [quest03/] ex06/ updated: finished My STRSTR.

--- ex06/my_strstr.c
+++ ex06/my_strstr.c
@@ -9,13 +9,54 @@
 **
 */
 
+#include <string.h>
+#include <stddef.h>
+//#include <stdio.h>
+
 char* my_strstr(char* param_1, char* param_2)
 {
-while letter_s1 in s1
-  while letter_s2 in s2
-    if letter_s1 != letter_s2
-      break
-  if reach end of s2
-    return &s1[index]
-return NULL
+	char* s_1 = param_1;
+	char* s_2 = param_2;
+	int s_1_n = strlen(s_1);
+	int s_2_n = strlen(s_2);
+//	int i = 0;
+//	int j = 0;
+
+	if ( !s_2_n )
+	{
+		return (char*) s_1;
+	}
+	while ( s_1_n >= s_2_n )
+	{
+		s_1_n--;
+		if ( !memcmp(s_1, s_2, s_2_n) )
+		{
+			return (char*) s_1;
+		}
+		s_1++;
+	}
+	return NULL;
 }
+
+/*int main()
+{
+	char* s_1_1 = "My name is Karl.";
+	char* s_1_2 = "K";
+	char* s_2_1 = "My name is Karl.";
+	char* s_2_2 = "Karl";
+	char* s_3_1 = "My name is Karl.";
+	char* s_3_2 = "a";
+	char* s_4_1 = "My name is Karl.";
+	char* s_4_2 = "ame";
+	char* s_5_1 = "My name is Karl.";
+	char* s_5_2 = "Z";
+	char* s_6_1 = "My name is Karl.";
+	char* s_6_2 = "Zorro";
+
+	printf("%s\n", my_strstr(s_1_1, s_1_2));
+	printf("%s\n", my_strstr(s_2_1, s_2_2));
+	printf("%s\n", my_strstr(s_3_1, s_3_2));
+	printf("%s\n", my_strstr(s_4_1, s_4_2));
+	printf("%s\n", my_strstr(s_5_1, s_5_2));
+	printf("%s\n", my_strstr(s_6_1, s_6_2));
+}*/
